a585e07a9325cac54a2cc1a2a3576d1c72862cac
Natalia <nataliamirandac@hotmail.com>
2020-05-15 11:28:38 -0400

    Store created and Provider wrapped the App inside index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//set up store and provider and middleware
+
+import { Provider } from "react-redux";
+import { createStore, applymidleware } from "redux";
+import thunk from "redux-thunk";
+import reducer from "./reducers";
+
+//create store and middleware
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
